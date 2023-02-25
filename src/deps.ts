d8db164ec93aae3bf911a0ee8b0e16001b9b13e2
Dominik.Kotecki <dominikus1910@gmail.com>
2023-02-25 11:59:59 +0100

    fix dependency managment

--- src/deps.ts
+++ src/deps.ts
@@ -1,3 +1,3 @@
-export { load } from "https://esm.sh/cheerio@1.0.0-rc.12";
-export type { CheerioAPI, Element } from "https://esm.sh/cheerio@1.0.0-rc.12";
-export { filter, from, mergeMap, toArray, Observable } from "npm:rxjs@7.6.0";
+export { load } from "cheerio";
+export type { CheerioAPI, Element } from "cheerio";
+export { filter, from, mergeMap, toArray, Observable } from "rxjs";
