bcf274f2d45ab7750458ffc20ff1c9ff7468e5ee
Dominik.Kotecki <dominikus1910@gmail.com>
2023-02-25 12:17:23 +0100

    add some tests

--- src/parser/page.ts
+++ src/parser/page.ts
@@ -1,7 +1,7 @@
 const url = "https://www.microsoft.com/pl-pl/store/deals/games/xbox";
 
 export const pageUrl = (page: number) => {
-  if (page === 0) {
+  if (page <= 1) {
     return url;
   }
   const skip = Math.ceil((page - 1) * 90);
--- src/parser/page_test.ts
+++ src/parser/page_test.ts
@@ -5,3 +5,13 @@ Deno.test(function pageZeroUrl() {
   const page = pageUrl(0)
   assertEquals("https://www.microsoft.com/pl-pl/store/deals/games/xbox", page)
 });
+
+Deno.test(function pageOneUrl() {
+    const page = pageUrl(1)
+    assertEquals("https://www.microsoft.com/pl-pl/store/deals/games/xbox", page)
+  });
+
+  Deno.test(function pageTwoUrl() {
+    const page = pageUrl(2)
+    assertEquals("https://www.microsoft.com/pl-pl/store/deals/games/xbox?skipitems=90", page)
+  });
